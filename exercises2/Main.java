2af6e9dce0af531909d936227e4347d99505e8b3
Catbug <kawtharinf@gmail.com>
2025-12-05 01:35:07 +0100

    Add files via upload
    Add Rectangle.java for exercise 2

---
+++ exercises2/Main.java
@@ -0,0 +1,10 @@
+package exercises.exercises2;
+
+public class Main {
+	public static void main(String[] args) {
+        Rectangle e1 = new Rectangle(5.0, 3.0);
+
+        System.out.println("Area: " + e1.calculateArea());
+        System.out.println("Perimeter: " + e1.calculatePerimeter());
+    }
+}
